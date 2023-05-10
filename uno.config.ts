9b5f986d4246e92d94699f142ec35b9365812c08
Christine Tham <chris.tham@hellotham.com>
2023-05-10 17:41:50 +1000

    Add typescript and vite

--- uno.config.ts
+++ uno.config.ts
@@ -1,6 +1,7 @@
 // uno.config.ts
 import {
   defineConfig,
+  presetWebFonts,
   presetAttributify, presetTypography, presetUno
 } from 'unocss'
 
@@ -8,6 +9,15 @@ export default defineConfig({
   presets: [
     presetAttributify(), // required when using attributify mode
     presetUno(), // required
+    presetWebFonts({
+      provider: 'google', // default provider
+      fonts: {
+        // these will extend the default theme
+        sans: ['Overpass', 'sans-serif'],
+        display: ['Playfair Display', 'serif'],
+        heading: ['Fira Sans Condensed', 'serif']
+      },
+    }),
     presetTypography(),
   ],
 })
