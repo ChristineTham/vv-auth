07ac458ac9819418dca35cbba2ed0e281f35e62b
Christine Tham <chris.tham@hellotham.com>
2023-05-12 10:52:33 +1000

    Netlify Identity

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
 // astro.config.ts
-import { defineConfig } from 'astro/config'
-import UnoCSS from 'unocss/astro'
+import { defineConfig } from 'astro/config';
+import UnoCSS from 'unocss/astro';
+import alpinejs from "@astrojs/alpinejs";
 
+// https://astro.build/config
 export default defineConfig({
   experimental: {
     assets: true
   },
   integrations: [
     UnoCSS({
-      injectReset: true // or a path to the reset file
-    }),
-  ],
-})
+    injectReset: true // or a path to the reset file
+  }),
+    alpinejs()
+  ]
+});
